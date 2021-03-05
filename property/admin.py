21c3e891ef1af845bf83fa10fae760e0c2f04264
Victor Shamakov <sva1.0@mail.ru>
2021-03-05 16:57:36 +0700

    Customize Flat Admin

--- property/admin.py
+++ property/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
+import sys
 
 from .models import Flat
 
+class FlatAdmin(admin.ModelAdmin):
+    search_fields = ['town', 'address', 'owner']
+    readonly_fields = ["created_at"]
 
-admin.site.register(Flat)
+admin.site.register(Flat, FlatAdmin)
+
+admin.site.site_header = "FLAT Admin"
+admin.site.site_title = "FLAT Admin Portal"
+admin.site.index_title = "Welcome to FLAT Portal"
