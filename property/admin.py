588f3dac3261b538c7b296ecdeb3e6030cbf3f38
Victor Shamakov <sva1.0@mail.ru>
2021-03-16 12:23:09 +0700

    Added complaints and likes

--- property/admin.py
+++ property/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 import sys
 
-from .models import Flat
+from .models import Flat, Complaint
 
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town', 'address', 'owner',)
@@ -9,5 +9,11 @@ class FlatAdmin(admin.ModelAdmin):
     list_display = ('price', 'new_building', 'construction_year',)
     list_editable = ('new_building',)
     list_filter = ['new_building', 'floor', 'rooms_number']
+    raw_id_fields = ('liked_by', )
 
 admin.site.register(Flat, FlatAdmin)
+
+class ComplaintAdmin(admin.ModelAdmin):
+    raw_id_fields = ('flat', )
+
+admin.site.register(Complaint, ComplaintAdmin)
--- property/models.py
+++ property/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Flat(models.Model):
@@ -48,6 +49,21 @@ class Flat(models.Model):
         db_index=True)
 
     new_building = models.NullBooleanField()
+    liked_by = models.ManyToManyField(User, related_name="liked_flats")
 
     def __str__(self):
         return f"{self.town}, {self.address} ({self.price}р.)"
+
+class Complaint(models.Model):
+    who = models.ForeignKey(User,
+                            related_name="complaints",
+                            verbose_name='Кто жаловался',
+                            on_delete=models.CASCADE)
+    flat = models.ForeignKey(Flat,
+                             related_name="flats",
+                             verbose_name='Квартира, на которую пожаловались',
+                             on_delete=models.CASCADE)
+    text = models.TextField('Текст жалобы')
+
+    def __str__(self):
+        return f"Жалоба от {self.who} на {self.flat}"
